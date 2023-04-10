6fa00d5fa94e72789cc70f9e2eabc47eb2081593
kelvin416 <kelvinrotich416@gmail.com>
2023-04-10 10:16:19 +0300

    a new commit

---
+++ src/labs_examples/fundamentals/labs/example.java
@@ -0,0 +1,18 @@
+package labs_examples.fundamentals.labs;
+
+public class example {
+
+    public static void main(String[] args) {
+
+        int result = multiply(33, 40);
+        System.out.println("Java is very fun to learn");
+        System.out.println("I can have the multiplication of 33 * 40: " + result);
+
+
+    }
+
+    public static int multiply(int a, int b){
+        int results = a * b;
+        return results;
+    }
+}
