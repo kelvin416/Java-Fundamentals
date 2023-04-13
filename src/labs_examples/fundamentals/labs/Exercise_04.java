5fc2ad29acb8194f4002833ece40b51be68af58d
kelvin416 <kelvinrotich416@gmail.com>
2023-04-13 16:03:57 +0300

    Labs complete

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,21 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        
+        //below are the additional data types
+        boolean isFull = true;
+        char letter = 'A';
+        short shortNum = 127;
+        long longNum = 1233232254;
+        float x = 234.9988f;
+        double d = 312.123786477;
+
+        System.out.println("The honey jar is full: " + isFull);
+        System.out.println("I like the letter: " + letter);
+        System.out.println("Very short number: " + shortNum);
+        System.out.println("Very long number: " + longNum);
+        System.out.println("This is a floating number: " + x);
+        System.out.println("This is a double number: " + d);
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,23 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int length = str.length();
+        System.out.println("The length of str is: " +length);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean equal = str.equalsIgnoreCase(str2);
+        System.out.println("str and str2 are the same? : " +equal);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String newString = str + " " +str2;
+        System.out.println("The two strings combined/concatenated makes: " + newString);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        System.out.println("ALL UPPERCASE" + str.toUpperCase());
+        System.out.println("all lowercase" + str.toLowerCase());
+
 
     }
 
