27211fc132ed49a8e3b9c28a76bb27f1a9bc997b
kelvin416 <kelvinrotich416@gmail.com>
2023-04-14 17:05:04 +0300

    Casting

---
+++ src/labs_examples/fundamentals/examples/casting/NarrowingConversion.java
@@ -0,0 +1,27 @@
+package labs_examples.fundamentals.examples.casting;
+
+public class NarrowingConversion {
+    public static void main(String[] args) {
+        // Narrowing Conversion / Explicit Casting
+        // The downside to explicit casting is information loss as you reduce the size of data type.
+        double d = 127.2321;
+        //double d is being cast to float f below - explicit casting required
+        float f = (float) d;
+        //float f is being cast to long l below - explicit casting required
+        long l = (long) f;
+        //long l is being cast to int i below - explicit casting required
+        int i = (int) l;
+        //int 'i' is being cast to short s below - explicit casting required
+        short s = (short) i;
+        //short s is being cast to byte b below - explicit casting required
+        byte b = (byte) s;
+
+        System.out.println("Double value: " + d);
+        System.out.println("Float value: " + f);
+        System.out.println("Long value: " + l);
+        System.out.println("Int value: " + i);
+        System.out.println("Short value: " + s);
+        System.out.println("Byte value: " + b);
+
+    }
+}
---
+++ src/labs_examples/fundamentals/examples/casting/WideningConversion.java
@@ -0,0 +1,21 @@
+package labs_examples.fundamentals.examples.casting;
+
+public class WideningConversion {
+    public static void main(String[] args) {
+        //widening casting / implicit casting /auto conversion of values
+        // variables are converted automatically as long as the data type is able to store the value of converted value.
+        int i = 99;
+        //conversion of int i to long j - no explicit type casting required.
+        long j = i;
+        //conversion of long j to float f - no explicit type casting required.
+        float f = j;
+        //conversion of float f to double d - no explicit type casting required.
+        double d = f;
+
+        System.out.println("Int Value is: " + i);
+        System.out.println("Long value is: " + j);
+        System.out.println("Float value is: " + f);
+        System.out.println("Double value is: " + d);
+
+    }
+}
