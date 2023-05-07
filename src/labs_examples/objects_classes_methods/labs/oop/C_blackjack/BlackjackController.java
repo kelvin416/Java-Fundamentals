8f92c1fbf246de8f49202f01edc912f76e606a3a
kelvin416 <kelvinrotich416@gmail.com>
2023-05-07 20:21:25 +0300

    BlackJack Ex1

---
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
@@ -0,0 +1,8 @@
+package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
+
+public class BlackjackController {
+    public static void main(String[] args) {
+        System.out.println("Welcome to BlackJack!");
+        Deck deck = new Deck();
+    }
+}
