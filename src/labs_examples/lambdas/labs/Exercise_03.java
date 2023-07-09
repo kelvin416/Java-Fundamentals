dba2625dcc3151e338fbe9288d56cacf46cd1277
kelvin416 <kelvinrotich416@gmail.com>
2023-07-09 19:33:55 +0300

    Exercise03 labs

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -8,3 +8,10 @@ package labs_examples.lambdas.labs;
  *      3) Demonstrate the use of a constructor reference
  *
  */
+
+//1
+class Fruits{
+    public static boolean isFruitsEnough(int a, int b){
+        return (a + b) > 50;
+    }
+}
