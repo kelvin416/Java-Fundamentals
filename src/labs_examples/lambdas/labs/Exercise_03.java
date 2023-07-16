0cf3fe55a84108231a8d96924fa44d95d179748e
kelvin416 <kelvinrotich416@gmail.com>
2023-07-16 15:19:33 +0300

    exercise_03 A

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -1,5 +1,7 @@
 package labs_examples.lambdas.labs;
 
+import java.util.function.BiFunction;
+
 /**
  * Lambdas Exercise 3:
  *
@@ -10,8 +12,14 @@ package labs_examples.lambdas.labs;
  */
 
 //1
-class Fruits{
-    public static boolean isFruitsEnough(int a, int b){
-        return (a + b) > 50;
+class Fruits {
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        BiFunction<Integer, Integer, Integer> fruitDivision = Fruits::divide;
+        int oranges = fruitDivision.apply(30, 20);
+        System.out.println("If you divide 30 fruits equally between 20 students you get a remainder of: " + oranges + " oranges");
     }
 }
