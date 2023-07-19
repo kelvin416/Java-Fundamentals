452ce3ef2d65144a7d74825b1afb1386a44f1f3c
kelvin416 <kelvinrotich416@gmail.com>
2023-07-19 09:54:35 +0300

    final lambda

--- src/labs_examples/lambdas/labs/Exe4Of8.java
+++ src/labs_examples/lambdas/labs/Exe4Of8.java
@@ -3,18 +3,16 @@ package labs_examples.lambdas.labs;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Exe4Of8 {
     public static void main(String[] args) throws IOException {
-//        double add = 0;
-//        String csvFile = "src/labs_examples/lambdas/labs/stream_text_lab.csv";
-//        Stream<String> sumOfStrings = Files.lines(Paths.get(csvFile));
-//        double sum = sumOfStrings
-//                .map(x -> x.split(","))
-//        System.out.println();
+        double add;
+        String csvFile = "src/labs_examples/lambdas/labs/stream_text_lab.csv";
+        Stream<String> sumOfStrings = Files.lines(Paths.get(csvFile));
+        sumOfStrings
+                .map(x -> x.split(","))
+                .filter(x -> Double.parseDouble(x[2]) > 0)
+                .forEach(x -> System.out.println(x[2]));
     }
 }
