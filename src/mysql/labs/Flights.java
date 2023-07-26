d0781def5b1fac153bc0a9e287692ed6f16eb097
kelvin416 <kelvinrotich416@gmail.com>
2023-07-26 22:17:25 +0300

    exe04_mysql

--- src/mysql/labs/Flights.java
+++ src/mysql/labs/Flights.java
@@ -1,2 +1,25 @@
-package mysql.labs;public class Flights {
+package mysql.labs;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Flights {
+    public static void main(String[] args) {
+
+    }
+
+    public void createFlight(){
+        Connection connection = null;
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
