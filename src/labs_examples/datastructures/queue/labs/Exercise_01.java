cf72a124a6c01fdc076bfbc8a1d94b20cd6d66c9
kelvin416 <kelvinrotich416@gmail.com>
2023-08-11 06:58:59 +0300

    Queue Labs EXE01

--- src/labs_examples/datastructures/queue/labs/Exercise_01.java
+++ src/labs_examples/datastructures/queue/labs/Exercise_01.java
@@ -1,5 +1,9 @@
 package labs_examples.datastructures.queue.labs;
 
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  *      Demonstrate your mastery of Java's (sun's) built-in Queue class (as seen in the import statement
  *      above) by completing the following:
@@ -7,3 +11,36 @@ package labs_examples.datastructures.queue.labs;
  *      1) instantiate a new Queue
  *      2) demonstrate the use of EVERY method in the Queue class - there are 7
  */
+
+class QueueController{
+    public static void main(String[] args) {
+        Queue<String> languages = new LinkedList<>();
+
+        languages.add("Java");
+        languages.add("JavaScript");
+        languages.add("Python");
+        languages.add("GoLang");
+        languages.add("Ruby");
+        languages.add("PHP");
+
+        System.out.println("The first language to learn is: " + languages.peek());
+
+        String element = languages.element();
+        System.out.println("Element is: " + element);
+
+        String removeElement = languages.remove();
+        System.out.println("Element that has been removed is: " + removeElement);
+
+        System.out.println("Current available languages: " +languages);
+
+        String pollElement = languages.poll();
+        System.out.println(pollElement);
+        System.out.println("Available languages after poll method: " + languages);
+
+        languages.offer("CSS");
+        System.out.println("Languages after additional add: " + languages);
+
+        System.out.println("The number of languages to be studied is: " + languages.size());
+
+    }
+}
